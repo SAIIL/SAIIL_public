935f140118696989b85e01d68b43ff94e22ba7ab
yutong-ban-1 <yutong.ban@gmail.com>
2020-11-28 12:39:36 -0500

    add setup file

---
+++ src/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from setuptools import find_packages, setup
+
+install_requires = []
+
+__version__ = '0.1.0'
+
+d = setup(
+    name='sdtm',
+    version=__version__,
+    packages=find_packages(exclude=['tests*', 'docs*']),
+    install_requires=install_requires,
+    author='Guy Rosman',
+    maintainer='Guy Rosman <rosman@csail.mit.edu>',
+    url='https://github.com/SAIIL/SAIIL_public',
+    keywords='laparoscopic,sdtm,model',
+    classifiers=['Environment :: Console'],
+    description="",
+    long_description="",
+    license='BSD',
+    test_suite='nose.collector',
+    tests_require=['nose'],
+    scripts=[],
+)
