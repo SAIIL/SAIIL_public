8b63b4dd7118026cb3ab1bf59e9a6d0d81f45eb6
Guy Rosman <guy.rosman@tri.global>
2020-12-07 23:49:30 -0500

    Cleaning up following TMW's comments

--- src/scripts/run_script_temporal_model.sh
+++ src/scripts/run_script_temporal_model.sh
@@ -11,5 +11,4 @@ sampling_rate=0.2
 ulimit -n 4096
 num_dataloader_workers_trainer=32
 
-
 python ${SAIIL_PUBLIC}/scripts/train_temporal_model.py --track_name phase --data_dir ${data_dir} --annotation_filename ${annotation_dir}/ --log_dir ${SAIIL_PUBLIC}/logs --learning_rate 4e-5 --training_ratio 1.0 --temporal_length 8 --sampling_rate $sampling_rate --model_filename ${SAIIL_PUBLIC}/temporal_weights/${script_arguments}_statedict.pt --cache_dir $cache_dir --num_dataloader_workers ${num_dataloader_workers_trainer} --batch_size 8
