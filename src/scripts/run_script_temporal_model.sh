86eee0ea183e4bb0c5709b8c339f95673f93f0f5
Guy Rosman <guy.rosman@tri.global>
2020-12-08 00:04:34 -0500

    Cleaning up following TMW's comments

--- src/scripts/run_script_temporal_model.sh
+++ src/scripts/run_script_temporal_model.sh
@@ -4,11 +4,11 @@ data_dir=~/SAIIL_public/data/cholec80/videos_processed
 annotation_dir=~/SAIIL_public/data/annotations/cholec80_protobuf
 cache_dir=~/cache_mgh/img_cache
 
-script_arguments=cnn_lstm
-echo $script_arguments
+model_filename=cnn_lstm
+echo $model_filename
 
 sampling_rate=0.2
 ulimit -n 4096
 num_dataloader_workers_trainer=32
 
-python ${SAIIL_PUBLIC}/scripts/train_temporal_model.py --track_name phase --data_dir ${data_dir} --annotation_filename ${annotation_dir}/ --log_dir ${SAIIL_PUBLIC}/logs --learning_rate 4e-5 --training_ratio 1.0 --temporal_length 8 --sampling_rate $sampling_rate --model_filename ${SAIIL_PUBLIC}/temporal_weights/${script_arguments}_statedict.pt --cache_dir $cache_dir --num_dataloader_workers ${num_dataloader_workers_trainer} --batch_size 8
+python ${SAIIL_PUBLIC}/scripts/train_temporal_model.py --track_name phase --data_dir ${data_dir} --annotation_filename ${annotation_dir}/ --log_dir ${SAIIL_PUBLIC}/logs --learning_rate 4e-5 --training_ratio 1.0 --temporal_length 8 --sampling_rate $sampling_rate --model_filename ${SAIIL_PUBLIC}/temporal_weights/${model_filename}_statedict.pt --cache_dir $cache_dir --num_dataloader_workers ${num_dataloader_workers_trainer} --batch_size 8
